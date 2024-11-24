f112445ffa4447ba7757d80edf48b59fb5189aa5
Luan Prudente <luanprudente.dev@gmail.com>
2024-11-24 14:29:52 -0300

    Correção da Logica

--- Avaliacao.py
+++ Avaliacao.py
@@ -51,40 +51,39 @@ def adicionar_cliente(cliente: Cliente):
 
 @app.put("/fila")
 def atualizar_fila():
-
     nao_atendidos = [cliente for cliente in fila_clientes if not cliente.atendido]
     prioritarios = [cliente for cliente in nao_atendidos if cliente.tipo_atendimento == AtendimentoTipo.prioritario]
     normais = [cliente for cliente in nao_atendidos if cliente.tipo_atendimento == AtendimentoTipo.normal]
-    
+
     nova_fila = []
-    prioridades_contagem = 0  
-    normais_contagem = 0     
+    prioridades_contagem = 0
 
     while prioritarios or normais:
-
         if prioritarios and prioridades_contagem < 2:
             nova_fila.append(prioritarios.pop(0))
             prioridades_contagem += 1
-
         elif normais and prioridades_contagem == 2:
             nova_fila.append(normais.pop(0))
             prioridades_contagem = 0
-
         elif prioritarios:
             nova_fila.append(prioritarios.pop(0))
             prioridades_contagem += 1
-
         elif normais:
             nova_fila.append(normais.pop(0))
+
 
     for cliente in nova_fila:
         cliente.posicao -= 1
         if cliente.posicao == 0:
             cliente.atendido = True
 
+
     fila_clientes[:] = [cliente for cliente in nova_fila if not cliente.atendido]
+
     atualizar_posicoes()
+
     return fila_clientes
+
 
 @app.delete("/fila/{id}", response_model=Cliente)
 def remover_cliente(id: int):
